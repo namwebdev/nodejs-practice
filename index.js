0e37cbbc2350edd41aed32ad46ee5ed41487118d
namwebdev <nam.job96@gmail.com>
2021-04-06 19:52:03 +0700

    - Edit server Port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require("./app/routes/root.router");
 const { sequelize } = require("./app/models/index");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Sever running at http://localhost:${port}/`);
