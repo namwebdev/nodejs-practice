440401c74da52b949b9da4bfdb3ba83b4714980a
namwebdev <nam.job96@gmail.com>
2021-04-22 15:10:04 +0700

    - Fix Sequelize config

--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require("sequelize");
 const { DB, HOST, USER, PASSWORD, dialect } = require("../../config/db.config");
 const { createProductModel } = require("./product.model");
+const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize(DB, USER, PASSWORD, {
   host: HOST,
   dialect,
+  dialectModule: mysql2,
 });
 
 const Product = createProductModel(sequelize);
